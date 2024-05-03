1e91f2f15e0b1839cadf476ef0b1105a148c7d9d
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-05-02 23:02:56 -0600

    Impresion de mensje por consola y funcion main.

---
+++ sec01_intro/_01_hello_world.swift
@@ -0,0 +1,11 @@
+/// HELLO WORLD
+///
+/// Imprimimos mensajes en consola empleando la función
+/// print.
+func main() {
+  // se muestra mensaje
+  print("Hello World in Swift!")
+}
+
+// Run application
+main()
