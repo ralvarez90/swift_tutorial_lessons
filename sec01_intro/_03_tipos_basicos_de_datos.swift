e763e538c3d8f8adfb603f24c6d391f811abfc98
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-05-02 23:07:06 -0600

    Intro a tipos de datos.

---
+++ sec01_intro/_03_tipos_basicos_de_datos.swift
@@ -0,0 +1,25 @@
+/// TIPOS DE DATOS BÁSICOS
+///
+/// Swift viene equipado con tipos de datos disponibles
+/// como enteros, flotantes, booleanos, etc.
+///
+/// Existen diversos tipos de datos numéricos como Int,
+/// UInt, entre otros.
+///
+/// Los flotantes no son mas que Float y Double.
+///
+/// Notas:
+/// - Existen elementos éstitcos que
+func main() {
+
+  // entero sin signo de 8 bits
+  let maxUInt8 = UInt8.max
+  let maxUInt64 = UInt64.max
+
+  // mostramos
+  print("maxUInt8  -> \(maxUInt8)")
+  print("maxUInt64 -> \(maxUInt64)")
+}
+
+// Run application
+main()
