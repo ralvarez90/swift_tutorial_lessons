b183346a6d949c42ab846c961c293cabb6eda8c3
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-05-03 19:37:34 -0600

    Conversiones de tipos numericos mediante constructor.

---
+++ sec01_intro/_05_conversiones_numeros.swift
@@ -0,0 +1,20 @@
+/// CONVERSIONES DE NÚMEROS
+///
+/// Podemos convertir valores numéricos a valores de otro tipo
+/// numérico empleando los nombres de las clases correspondientes.
+/// 
+/// De forma general, la conversión de tipos numéricos es de la
+/// forma SomeType(ofInitialValue)
+func main() {
+
+  // entero sin singo
+  let someInt: UInt8 = 1
+  var otherInt: UInt64 = UInt64(someInt)
+  print("otherInt: \(otherInt) with type:", type(of: otherInt))
+
+  otherInt = 123
+  print("someInt + otherInt = \(someInt + UInt8(otherInt))")
+}
+
+// Run application
+main()
